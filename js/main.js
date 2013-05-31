0b9857798bce23a1ae5514e6921da02e56784439
Oscar Paucara <oscar.paucara@globant.com>
2013-05-31 17:08:07 -0300

    Using shim for load dependencias for _

--- js/main.js
+++ js/main.js
@@ -3,12 +3,18 @@ require.config({
     jquery: 'lib/jquery-1.7.2.min',
     underscore: 'lib/underscore-min',
     backbone: 'lib/backbone-min'
-  }
+  },
+  shim: {
+        backbone: {
+            deps: ['jquery','underscore'],
+            exports: 'Backbone'
+        }
+    }
 });
 
 require([/*'lib/backbone-min',
 		'lib/underscore-min',*/
-		'app/view/appView'], function(appView) {
+		'app/view/appView'], function(AppView) {
     //This function is called when scripts/widgets/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
